6c857c20f0780d6a9cb1eb4b83b55f42fc901378
Connor Stevens <conmail1224@gmail.com>
2023-05-06 14:01:04 -0400

    Attempt to fix

--- monitoring/system.py
+++ monitoring/system.py
@@ -1,5 +1,7 @@
 import subprocess
 
+bin_bash = "/bin/bash"
+
 
 def get_hostname():
     hostname = subprocess.getoutput("hostname")
@@ -9,7 +11,7 @@ def get_hostname():
 
 def get_interface():
     interface = subprocess.getoutput(
-        "ifconfig | grep \"ens\" | awk -F [:] '{print $1}'")
+        [bin_bash, "ifconfig | grep \"ens\" | awk -F [:] '{print $1}'"])
 
     return interface
 
@@ -18,47 +20,47 @@ def get_mac_address():
     interface = get_interface()
 
     command = f"ifconfig {interface} | grep \"ether\" | awk '{{print $2}}'"
-    mac_address = subprocess.getoutput(command)
+    mac_address = subprocess.getoutput([bin_bash, command])
 
     return mac_address
 
 
 def get_cpu_cores():
-    cores = int(subprocess.check_output(["nproc"]))
+    cores = int(subprocess.check_output([bin_bash, "nproc"]))
 
     return cores
 
 
 def get_cpu_utilization():
     cpu_utilization = int(subprocess.check_output(
-        "top -b -n 1 -U $(whoami) | grep \"$(whoami)\" | awk '{total+=$9} END {print total}'"))
+        [bin_bash, "top -b -n 1 -U $(whoami) | grep \"$(whoami)\" | awk '{total+=$9} END {print total}'"]))
 
     return cpu_utilization / 100
 
 
 def get_memory_amount():
     memory_amount = int(subprocess.check_output(
-        "lsmem | grep \"Total online\" | awk {print $4} | tr -d \"G\" ")) * 1024
+        [bin_bash, "lsmem | grep \"Total online\" | awk {print $4} | tr -d \"G\""])) * 1024
 
     return memory_amount
 
 
 def get_memory_utilization():
     memory_utilization = int(subprocess.check_output(
-        "top -b -n 1 -U $(whoami) | grep \"$(whoami)\" | awk '{total+=$10} END {print total}'"))
+        [bin_bash, "top -b -n 1 -U $(whoami) | grep \"$(whoami)\" | awk '{total+=$10} END {print total}'"]))
 
     return memory_utilization
 
 
 def get_disk_size():
     disk_size = subprocess.getoutput(
-        "df -h |  grep \"boot$\" | awk '{print $5}' | tr -d '%'")
+        [bin_bash, "df -h |  grep \"boot$\" | awk '{print $5}' | tr -d '%'"])
 
     return disk_size
 
 
 def get_disk_utilization():
     disk_utilization = subprocess.getoutput(
-        "df -h |  grep \"boot$\" | awk '{print $2}'")
+        [bin_bash, "df -h |  grep \"boot$\" | awk '{print $2}'"])
 
     return disk_utilization
